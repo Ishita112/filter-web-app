9778d76ca19b33199ad942badb965df76b2649a5
Ishita112 <109744787+Ishita112@users.noreply.github.com>
2022-10-18 18:30:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,23 @@ function setup() {
     canvas.center();
 }
 
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!')
+}
+
 function draw() {
+    image(video, 0, 0, 300, 300);
 
 }
 
